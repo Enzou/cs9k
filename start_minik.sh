2da5ad9016e8ccede8b2281f7552b6a2f2257610
markus_gierlinger <S1510239009@students.fh-hagenberg.at>
2020-02-02 10:24:17 +0100

    created cusotm container for adversaries

--- start_minik.sh
+++ start_minik.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-minikube status
+status=$(minikube status | grep host | cut -d " " -f2)
+
+if [ "$status" == "Stopped" ]; then 
+    minikube start
+fi
 
 minikube addons enable ingress
 
 minikube tunnel &> /dev/null &
 echo "Enabled tunnel mode"
+
+minikube status
 
 minikube dashboard
 
