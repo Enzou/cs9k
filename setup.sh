bace743b8ead2870ee238ab7d21f1d82a320433c
bluza <bernegfa@gmail.com>
2020-02-09 23:33:45 +0100

    working state

--- setup.sh
+++ setup.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+## run this after setting up kiali 
+
 NS=battlefield
 
-kubectl apply -f environment/$NS.yaml
-kubectl config set-context --current --namespace=$NS
+sudo kubectl apply -f environment/$NS.yaml
+sudo kubectl config set-context --current --namespace=$NS
 
-kubectl apply -f environment/victim.yaml
-kubectl apply -f environment/caldera.yaml
-kubectl apply -f environment/frigate.yaml
+# start services
+sudo kubectl apply -f environment/victimv2.yaml
+sudo kubectl apply -f environment/caldera.yaml
+sudo kubectl apply -f environment/frigate.yaml
+
+# network
+sudo kubectl apply -f environment/network/destination_rules.yaml 
+sudo kubectl apply -f environment/network/virtual_services.yaml 
+
+# telemetrie
+
+sudo kubectl apply -f environment/telemetrie/caldera.yaml 
