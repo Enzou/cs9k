59953daec7625b98c22c758ec368d426b7cdd5c3
markus_gierlinger <S1510239009@students.fh-hagenberg.at>
2020-02-10 09:00:51 +0100

    removed 'sudo' from scripts

--- kiali_init_setup.sh
+++ kiali_init_setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # install istio in the cluster
-sudo istioctl manifest apply 
+istioctl manifest apply 
 
 # add namespace battlefield
-sudo kubectl apply -f environment/battlefield.yaml
+kubectl apply -f environment/battlefield.yaml
 
 # activate auto sidecar injection
-sudo kubectl label namespace battlefield istio-injection=enabled
+kubectl label namespace battlefield istio-injection=enabled
--- setup.sh
+++ setup.sh
@@ -4,18 +4,18 @@
 
 NS=battlefield
 
-sudo kubectl apply -f environment/$NS.yaml
-sudo kubectl config set-context --current --namespace=$NS
+kubectl apply -f environment/$NS.yaml
+kubectl config set-context --current --namespace=$NS
 
 # start services
-sudo kubectl apply -f environment/victimv2.yaml
-sudo kubectl apply -f environment/caldera.yaml
-sudo kubectl apply -f environment/frigate.yaml
+kubectl apply -f environment/victimv2.yaml
+kubectl apply -f environment/caldera.yaml
+kubectl apply -f environment/frigate.yaml
 
 # network
-sudo kubectl apply -f environment/network/destination_rules.yaml 
-sudo kubectl apply -f environment/network/virtual_services.yaml 
+kubectl apply -f environment/network/destination_rules.yaml 
+kubectl apply -f environment/network/virtual_services.yaml 
 
 # telemetrie
 
-sudo kubectl apply -f environment/telemetrie/caldera.yaml 
+kubectl apply -f environment/telemetrie/caldera.yaml 
